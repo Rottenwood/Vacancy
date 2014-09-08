b30ff731dde9d3b4b297f0209fd86856762fcdda
Petr Karmashev (Rottenwood) <petr@quank.org>
2014-09-08 17:05:13 +0400

    Создание проекта "vacancy"

---
+++ src/Rottenwood/VacancyBundle/Controller/DefaultController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Rottenwood\VacancyBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class DefaultController extends Controller
+{
+    public function indexAction()
+    {
+        return $this->render('RottenwoodVacancyBundle:Default:index.html.twig');
+    }
+}
---
+++ src/Rottenwood/VacancyBundle/RottenwoodVacancyBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Rottenwood\VacancyBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class RottenwoodVacancyBundle extends Bundle
+{
+}

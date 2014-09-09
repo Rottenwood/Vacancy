6a4587e6dfd5596b7e7408ad8516efcbb3cc20f8
Petr Karmashev (Rottenwood) <petr@quank.org>
2014-09-10 02:30:48 +0400

    Удалены лишние файлы

--- src/Rottenwood/VacancyBundle/Resources/views/Default/layout.html.php
+++ src/Rottenwood/VacancyBundle/Resources/views/Default/layout.html.php
@@ -5,18 +5,14 @@
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
     <!-- jQuery -->
     <script type="text/javascript" src="<?php echo $view['assets']->getUrl('assets/js/jquery.min.js') ?>"></script>
-    <!-- <script src="http://malsup.github.com/jquery.form.js"></script> -->
     <!-- Styles -->
     <link type="text/css" href="<?php echo $view['assets']->getUrl('assets/css/css-reset.css') ?>" rel="stylesheet"/>
     <link type="text/css" href="<?php echo $view['assets']->getUrl('assets/css/style.css') ?>" rel="stylesheet"/>
     <link type="text/css" href="<?php echo $view['assets']->getUrl('assets/css/bootstrap.min.css') ?>" rel="stylesheet"/>
     <link type="text/css" href="<?php echo $view['assets']->getUrl('assets/css/bootstrap-theme.min.css') ?>" rel="stylesheet"/>
-    <!-- Fonts -->
-    <link type="text/css" href="<?php echo $view['assets']->getUrl('assets/css/font-awesome.css') ?>" rel="stylesheet"/>
     <!-- Scripts -->
     <script type="text/javascript" src="<?php echo $view['assets']->getUrl('assets/js/script.js') ?>"></script>
     <script type="text/javascript" src="<?php echo $view['assets']->getUrl('assets/js/bootstrap.js') ?>"></script>
-    <script type="text/javascript" src="<?php echo $view['assets']->getUrl('assets/js/bootstrap-confirmation.js') ?>"></script>
 </head>
 <body>
 <?php $view['slots']->output('_content') ?>
